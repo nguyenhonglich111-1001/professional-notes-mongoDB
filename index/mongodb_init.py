3be0fe983e97192d748e6d0dc1a43e259d4a68c1
Lich <lichnguyenhong.it@gmail.com>
2022-12-21 23:43:40 +0700

    feat: index

---
+++ index/mongodb_init.py
@@ -0,0 +1,33 @@
+import pymongo
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote_plus
+load_dotenv()
+
+mongodb_password = os.environ['mongodb_password']
+mongodb_password = quote_plus(mongodb_password)
+
+connect_string = f'mongodb+srv://hdttuan:{mongodb_password}@cluster0.h09da4d.mongodb.net/?retryWrites=true&w=majority'
+
+crawlClient = pymongo.MongoClient(connect_string)
+crawlDB = crawlClient['TRACKINGINVESTMENT_CRAWL']
+
+personal_mongodb_password = os.environ['personal_mongodb_password']
+personal_mongodb_password = quote_plus(personal_mongodb_password)
+
+personal_connection_string = f'mongodb+srv://lichnh:{personal_mongodb_password}@personal.r7jxpl0.mongodb.net/?retryWrites=true&w=majority'
+
+personal_client = pymongo.MongoClient(personal_connection_string)
+codingDB = personal_client['coding']
+employeeDocs = codingDB['employees']
+cardDocs = codingDB['cards']
+
+
+
+
+
+
+
+
+
+
